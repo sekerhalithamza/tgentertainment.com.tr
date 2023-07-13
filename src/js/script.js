74c164b2915edc05499055b5d08e4f2c61adee78
Halit Hamza Şeker <shalithamza@gmail.com>
2023-07-13 12:10:10 +0300

    add: pop up
    @Notenlish

--- src/js/script.js
+++ src/js/script.js
@@ -4,6 +4,8 @@ const languageBtn = document.getElementById("languageBtn");
 const textElements = document.getElementsByClassName("text");
 const chevronBtns = document.getElementsByClassName("section-hero--chevron");
 const images = document.getElementsByClassName("section-hero__container");
+const serviceContainers = document.getElementsByClassName("section-services__container-content");
+const popUp = document.getElementById("popUp");
 
 languageBtn.addEventListener("click", function () {
   for (const element of textElements) {
@@ -30,3 +32,28 @@ for (const btn of chevronBtns) {
     }
   });
 }
+
+for (const container of serviceContainers) {
+  container.addEventListener("click", function () {
+    const title = container.getElementsByClassName(
+      "section-services__container-content-wrapper--title"
+    )[0];
+    const text = container.getElementsByClassName(
+      "section-services__container-content-wrapper--text"
+    )[0];
+
+    const popUpTitle = popUp.getElementsByClassName("popup__content--title")[0];
+    const popUpText = popUp.getElementsByClassName("popup__content--text")[0];
+
+    [popUpTitle.innerText, popUpText.innerText] = [title.innerText, text.innerText];
+    popUpTitle.setAttribute("data-language", title.getAttribute("data-language"));
+    popUpText.setAttribute("data-language", text.getAttribute("data-language"));
+    popUp.classList.add("active");
+  });
+}
+
+popUp.addEventListener("click", (event) => {
+  if (!event.target.closest(".popup__content")) {
+    popUp.classList.remove("active");
+  }
+});
